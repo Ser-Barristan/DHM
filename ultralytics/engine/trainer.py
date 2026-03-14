252d4f0f90ef5596727dfbdb2a1f59f82a836b22
Ser-Barristan <ranjanayush496@gmail.com>
2026-03-15 00:15:00 +0530

    Update trainer.py
    Signed-off-by: Ser-Barristan <ranjanayush496@gmail.com>

--- ultralytics/engine/trainer.py
+++ ultralytics/engine/trainer.py
@@ -318,7 +318,7 @@ class BaseTrainer:
             self.validator = self.get_validator()
             metric_keys = self.validator.metrics.keys + self.label_loss_items(prefix="val")
             self.metrics = dict(zip(metric_keys, [0] * len(metric_keys)))
-            self.ema = ModelEMA(self.model)
+            self.ema = None
             if self.args.plots:
                 self.plot_training_labels()
 
