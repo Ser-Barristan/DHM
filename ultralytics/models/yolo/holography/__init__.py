b5756955413c8ae6f728a124a1ebc7451690b24a
Ser-Barristan <212214432+Ser-Barristan@users.noreply.github.com>
2026-05-25 16:59:18 +0530

    Update __init__.py
    Signed-off-by: Ser-Barristan <212214432+Ser-Barristan@users.noreply.github.com>

--- ultralytics/models/yolo/holography/__init__.py
+++ ultralytics/models/yolo/holography/__init__.py
@@ -1,4 +1,5 @@
 # ultralytics/models/yolo/holography/__init__.py
 from .train import HolographyTrainer
 from .val   import HolographyValidator
+
 __all__ = ['HolographyTrainer', 'HolographyValidator']
